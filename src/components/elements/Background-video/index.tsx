021c15e16db29c14c4edbc2d943c9426e51ff37b
rizkyramadika <rizky@liquidx.studio>
2025-01-18 20:54:21 +0700

    update video

--- src/components/elements/Background-video/index.tsx
+++ src/components/elements/Background-video/index.tsx
@@ -6,7 +6,7 @@ const LandingPage = ({ children }: any) => {
       {/* Background Video */}
       <video
         className="absolute top-0 left-0 w-full h-full object-cover"
-        src="https://res.cloudinary.com/dp5hwzmhm/video/upload/v1736911429/2169880-uhd_3840_2160_30fps_1_1_hcvsjw.mp4"
+        src="https://res.cloudinary.com/dp5hwzmhm/video/upload/v1737208326/hero-bg-video_vxrqxx.mp4"
         autoPlay
         loop
         muted
